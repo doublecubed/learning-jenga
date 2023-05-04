7c36d488b6e2d733626238900b38129c754465c3
doublecubed <onurereren@gmail.com>
2023-05-04 20:46:39 +0300

    Initial commit
    * Project creation
    * json import

---
+++ Assets/Scripts/BlockData.cs
@@ -0,0 +1,16 @@
+namespace JengaGame.Data
+{
+
+    public struct BlockData
+    {
+        public int Id;
+        public string Subject;
+        public string Grade;
+        public int GradeLevel;
+        public int Mastery;
+        public string DomainId;
+        public string Cluster;
+        public string StandardId;
+        public string StandardDescription;
+    }
+}
