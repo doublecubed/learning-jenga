6f71d68dfcc2640b593396a3749014e97a0b40a0
doublecubed <onurereren@gmail.com>
2023-05-07 17:14:27 +0300

    Camera clamps added

--- Assets/Scripts/Gameplay/CameraController.cs
+++ Assets/Scripts/Gameplay/CameraController.cs
@@ -5,8 +5,11 @@
 // Camera controller script. Listens to mouse input as well as 6-7-8 on the number line
 // Performs camera movements and also makes switches between tower focus points.
 
+using System;
 using System.Collections;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 namespace JengaGame.Gameplay
 {
@@ -21,18 +24,34 @@ namespace JengaGame.Gameplay
 		
 		#region VARIABLES
 		
+		[Header("Camera Movement Parameters")]
 		public float VerticalMoveSpeed;
 		public float HorizontalRotateSpeed;
 		public float ZoomSpeed;
 		public float FocusSwitchDuration;
+
+		[Header("Camera Movement Restrictions")]
+		public float CameraMaxHeight;
+		public float CameraMinHeight;
+		[Space(5)] 
+		public float CameraMaxDistance;
+		public float CameraMinDistance;
+		
 		
 		private bool _mousePressed;
 		private bool _cameraMoving;
-
+		private int _currentFocusCenter;
+		
 		#endregion
 		
 		#region MONOBEHAVIOUR
-		
+
+		private void Start()
+		{
+			// The camera looks at 7th tower at the beginning since it is the center.
+			_currentFocusCenter = 1;
+		}
+
 		private void Update()
 		{
 			ResolveMousePresses();
@@ -61,6 +80,7 @@ namespace JengaGame.Gameplay
 
 		private void ResolveCameraMovement()
 		{
+			// Rotational Movement
 			if (_mousePressed && !_cameraMoving)
 			{
 				float mouseXDelta = Input.GetAxis("Mouse X");
@@ -68,13 +88,23 @@ namespace JengaGame.Gameplay
 
 				
 				transform.Rotate(0f, mouseXDelta * HorizontalRotateSpeed, 0f);
-				cameraTransform.position += Vector3.up * mouseYDelta * VerticalMoveSpeed * Time.deltaTime;
+				
+				Vector3 cameraDesiredPosition = cameraTransform.position + Vector3.up * mouseYDelta * VerticalMoveSpeed * Time.deltaTime;
+				float clampedYPosition = Mathf.Clamp(cameraDesiredPosition.y, CameraMinHeight, CameraMaxHeight);
+
+				cameraTransform.position =
+					new Vector3(cameraDesiredPosition.x, clampedYPosition, cameraDesiredPosition.z);
 			}
 			
-			
+			// Zoom Movement
 			float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
-			cameraTransform.position += cameraTransform.forward * mouseScroll * ZoomSpeed;
+			Vector3 cameraZoomPosition = cameraTransform.position + cameraTransform.forward * mouseScroll * ZoomSpeed;
+			Vector3 axisCenter = DistanceMeasurementPoint(focusPointCenters[_currentFocusCenter]);
+			Vector3 cameraDistanceVector =
+				ClampedRadialVector(axisCenter, cameraZoomPosition);
+			cameraTransform.position = axisCenter + cameraDistanceVector;
 		}
+		
 
 		private void ResolveGradeButtonPresses()
 		{
@@ -97,29 +127,48 @@ namespace JengaGame.Gameplay
 		
 		public void MoveToSixthGrade()
 		{
-			StartCoroutine(MoveToFocus(focusPointCenters[0]));
+			StartCoroutine(MoveToFocus(0));
 		}
 
 		public void MoveToSeventhGrade()
 		{
-			StartCoroutine(MoveToFocus(focusPointCenters[1]));
+			StartCoroutine(MoveToFocus(1));
 		}
 
 		public void MoveToEighthGrade()
 		{
-			StartCoroutine(MoveToFocus(focusPointCenters[2]));
+			StartCoroutine(MoveToFocus(2));
 		}
 
+		private Vector3 ClampedRadialVector(Vector3 centerPoint, Vector3 satellitePoint)
+		{
+			Vector3 radialVector = satellitePoint - centerPoint;
+
+			if (radialVector.magnitude < CameraMinDistance)
+			{
+				return radialVector.normalized * CameraMinDistance;
+			}
+			return Vector3.ClampMagnitude(radialVector, CameraMaxDistance);
+
+		}
+		
+		private Vector3 DistanceMeasurementPoint(Transform focusPointCenter)
+		{
+			return new Vector3(focusPointCenter.position.x, cameraTransform.position.y, focusPointCenter.position.z);
+		}
+		
 		#endregion
 		
 		#region COROUTINES
 		
-		private IEnumerator MoveToFocus(Transform focusPoint)
+		private IEnumerator MoveToFocus(int focusIndex)
 		{
 			_cameraMoving = true;
+
+			_currentFocusCenter = focusIndex;
 			
 			Vector3 startPos = transform.position;
-			Vector3 endPos = focusPoint.position;
+			Vector3 endPos = focusPointCenters[focusIndex].position;
 			
 			float timer = 0f;
 			while (timer < FocusSwitchDuration)
